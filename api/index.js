05b4f1148c8a47daca2cf4ada4daf4ce25515844
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-27 12:10:59 -0300

    setup to deploy render

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const exphbs = require("express-handlebars");
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 const rutas = require("./routes");
 app.engine(
   ".hbs",
